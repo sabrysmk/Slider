41dda2bc5fd11ade425b5f6b8ec1188ae4ca5613
Aleksandr Sabry <sabry.smk@gmail.com>
2020-05-13 20:27:55 +0300

    try to add spm

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "Slider",
+    platforms: [
+        .iOS(.v9),
+        .tvOS(.v9)
+    ],
+    products: [
+        .library(name: "Slider", targets: ["Slider"]),
+    ],
+    targets: [
+        .target(name: "Slider", path: "Slider/")
+    ],
+    swiftLanguageVersions: [
+        .v5
+    ]
+)
